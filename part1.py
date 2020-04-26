89397d5abeb704023bd99f33ad00643073f974b8
lexboom <47512002+lexboom@users.noreply.github.com>
2020-04-26 18:27:06 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	part1.py

--- part1.py
+++ part1.py
@@ -32,89 +32,82 @@ class Hero(Character):
 
 
 class Enemy(Character):
-   def __init__(self,name,initialHealth,damageAmount):
+    def __init__(self,name,initialHealth,damageAmount):
        Character.__init__(self,name,initialHealth)
        self.damageAmount = damageAmount
-
 if __name__ == "__main__":
-   # Our Hero Geralt
-   Geralt = Hero("Geralt",40)
-   Geralt.add_inventory("50 gold coins")
+    Geralt = Hero("Geralt",40)
+    Geralt.add_inventory("50 gold coins")
 
    # Vampire Enemy with health of 20 and damage of 15.
-   Vampire = Enemy("Vampire",30,20)
+    Vampire = Enemy("Vampire",30,20)
 
    # Create an Enemy named “Werewolf” with health of 15 and damage of 10.
-   Werewolf = Enemy("Werewolf",15,10)
+    Werewolf = Enemy("Werewolf",15,10)
 
-   # Print “Start:” and then print the three characters.
-   print("“Our Hero Geralt finds himself in Skellige Isles in search of a boat to get him to Novigrad.")
-   Geralt.__str__()
-  ## Talking to the captain ##
-   print('Press (T) to talk to a captain')
-   key = input()
-
-   if key == "T" or "t":
-       print('What ye bothering me fer?')
-
-       print('Geralt: "I Need a ride to Novigrad"')
-       print('''Captain: "That'll cost ye 10 gold"''')
-       print('Press (I) to view inventory')
-       key2 = input()
-
-       if key2 == "I" or "i":
-         print(Geralt.get_inventory())
-         print('Press (B) to buy a fare')
-         key3 = input()
-       else:
-           print('Press (I) to view inventory')
-
-
-       if key3 == 'B' or 'b':
-           Geralt.remove_inventory("50 gold coins")
-           Geralt.add_inventory("40 gold coins")
-           print(Geralt.get_inventory())
-           print('Geralt Sails his way across the rough seas determined to make it to Novigrad in the hopes of a long awaited reunion with Jennefer')
-       else:
-           print('Press (B) to buy a fare')
-   else:
-       print('Press (T) to talk to captain')
+   # Print “Start:” and then print the three characters.#
+print("“Our Hero Geralt finds himself in Skellige Isles in search of a boat to get him to Novigrad.")
+Geralt.__str__()
+   ## Talking to the captain ##
+print('Press (T) to talk to a captain')
+key = input()
+if key == "T" or "t":
+    print('What ye bothering me fer?')
+    print('Geralt: "I Need a ride to Novigrad"')
+    print('''Captain: "That'll cost ye 10 gold"''')
+    print('Press (I) to view inventory')
+    key2 = input()
+    if key2 == "I" or "i":
+        print(Geralt.get_inventory())
+        print('Press (B) to buy a fare')
+        key3 = input()
+        if key3 == 'B' or 'b':
+            Geralt.remove_inventory("50 gold coins")
+            Geralt.add_inventory("40 gold coins")
+            print(Geralt.get_inventory())
+            print('Geralt Sails his way across the rough seas determined to make it to Novigrad in the hopes of a long awaited reunion with Jennefer')
+        else:
+          print('Press (B) to buy a fare')
+    else:
+          print('Press (I) to view inventory')
+else:
+      print('Press (T) to talk to captain')
 
 
 print('Geralt makes his way to the shores of the Mainland. As he and his stead Roach traverse the backwoods paths he can smell something is lurking near by. Geralt makes an attempt to speed roach up in an attempt to outrun the mysteryious beast. He can hear branches breaking  behind him run from the trees appears a Werewolf. Geralt has no choice but to make a stand against the monster.')
 
    # Print Battle 1 and then print the hero and the werewolf.
-print('Press "'"a"'" to atatck the Werewolf')
+print('Press (a) to attack the Werewolf')
 key4 = input()
 if key4 == 'a' or 'A':
-
-   Geralt.take_damage(Werewolf.damageAmount)
-   Werewolf.take_damage(15)
-   print(' The Werewolf damages the hero by its full damage strength. Geralt damages the Werewolf critcally')
-   Geralt.__str__()
-   Werewolf.__str__()
-   Geralt.add_inventory("Werewolf Claws")
-   Geralt.remove_inventory("40 gold coins")
-   Geralt.add_inventory("50 gold coins")
-   print("Inventory: ", Geralt.get_inventory())
-
+     Geralt.take_damage(Werewolf.damageAmount)
+     Werewolf.take_damage(15)
+     print(' The Werewolf damages the hero by its full damage strength. Geralt damages the Werewolf critcally')
+     Geralt.__str__()
+     Werewolf.__str__()
+     Geralt.add_inventory("Werewolf Claws")
+     print('Geralt picks up Werewolf Claws')
+     print("Inventory: ", Geralt.get_inventory())
+else:
+     print('Press "'"a"'" to attack the Werewolf')
    # Print “Battle 2:” and then print the hero and the zombie.
-   print("Battle 2:")
-   # An encounter occurs between the hero and the zombie. The hero damages the zombie by 20.
-   # The zombie damages the hero by its full damage strength.
-   Geralt.take_damage(Vampire.damageAmount)
-   Vampire.take_damage(20)
-   Geralt.__str__()
-   Vampire.__str__()
+
+print('Geralt continues on this path to novigrad when he stumbles upon a wormwood tree')
+
+print("Battle 2:")
+Geralt.take_damage(Vampire.damageAmount)
+Vampire.take_damage(20)
+Geralt.__str__()
+Vampire.__str__()
 
    # Print “Restore Health” and then print the hero
-   print("Restore Health: ")
-   Geralt.restore_health(5)
-   Geralt.__str__()
+print("Restore Health: ")
+Geralt.restore_health(5)
+Geralt.__str__()
 
    # Print “Inventory:” and then print the hero’s inventory by using the get_inventory method.
-   print("Inventory: ")
-   Geralt.add_inventory("gold coin")
-   Geralt.add_inventory("Vampire Fangs")
-   print(Geralt.get_inventory())
+print("Inventory: ")
+Geralt.add_inventory("gold coin")
+Geralt.add_inventory("Vampire Fangs")
+print(Geralt.get_inventory())
 
